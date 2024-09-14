b5595a15b0ddff567aca0d5cabc19cccdbde7754
Isaak <isaac@thesheridans.email>
2024-09-14 17:39:58 -0400

    working commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,51 @@
+import HeroCarousel from '@/components/HeroCarousel'
+import SearchBar from '@/components/SearchBar'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='px-6  md:px-20 py-24 '>
+        <div className='flex max-xl:flex-col gap-16'>
+          <div className='flex flex-col justify-center '>
+            <p className='small-text'>
+              Smart Purchasing begins with MoneyBall
+              <Image 
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className='head-text'>
+              Unleash the power of 
+              <span className='text-green-700'> MoneyBall</span>
+            </h1>
+
+            <p className='mt-6'>
+              Powerful product scraping tools to grow your business
+            </p>
+
+            <SearchBar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+
+      </section>
+
+      <section className='trending-section'>
+        <h2 className='section-text'>Trending</h2>
+
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+          {['Apple Iphone 16', 'book', 'sneakers'].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
   )
 }
 
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,76 +1,72 @@
-"use client" 
-import { scrapeAndStoreProduct } from '@/lib/actions'
-import { parseUrl } from 'next/dist/shared/lib/router/utils/parse-url'
-import React, {FormEvent, useState} from 'react'
+"use client"
+
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react'
 
 const isValidAmazonProductURL = (url: string) => {
   try {
-    const parsedURL = new URL(url)
-    const hostname = parsedURL.hostname
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
 
-      if (hostname.includes('amazon.com') ||
-          hostname.includes('amazon') ||
-          hostname.endsWith('amazon')
-      ) {return true}
-
+    if(
+      hostname.includes('amazon.com') || 
+      hostname.includes ('amazon.') || 
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
   } catch (error) {
-    return false
+    return false;
   }
-  return false
+
+  return false;
 }
 
-const SearchBar = () => {
-
- 
-  
-
-  const [searchPrompt, setSearchPrompt] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const isValidLink = isValidAmazonProductURL(searchPrompt)
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    if (!isValidLink) {
-      return alert('Please enter a valid Amazon Product URL')
-    }
-    
+    if(!isValidLink) return alert('Please provide a valid Amazon link')
+
     try {
-      setIsLoading(true)
+      setIsLoading(true);
 
-      // scrape the product page
-      const product = await scrapeAndStoreProduct(searchPrompt)
-
+      // Scrape the product page
+      const product = await scrapeAndStoreProduct(searchPrompt);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
+  }
 
   return (
-    <form
-     className='flex flex-wrap gap-4 mt-12'
-      onSubmit={handleSubmit}>
+    <form 
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
 
-        <input
-          type='text'
-          value={searchPrompt}
-          onChange={(e) => setSearchPrompt(e.target.value)}
-          className='searchbar-input'
-          placeholder='Enter product link'
-        />
-        <button 
-          type='submit' 
-          className='searchbar-btn'
-          disabled ={searchPrompt === ''}  
-          >
-          {isLoading ? 'Searching...' : 'Search'}
-        </button>
-  
+      <button 
+        type="submit" 
+        className="searchbar-btn"
+        disabled={searchPrompt === ''}
+      >
+        {isLoading ? 'Searching...' : 'Search'}
+      </button>
     </form>
   )
 }
-}
 
-export default SearchBar
+export default Searchbar
